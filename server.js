dec6224b28ea08239596685b5a74a3dba17ae934
elsie20 <elsie.ahema@acity.edu.gh>
2025-12-14 22:32:33 +0000

    new

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.use(express.urlencoded({ extended: true }));
 
 
 server.get('/', (req, res) => {
-  res.json({ message: 'Backend API running ' });
+  res.json({ message: 'Backend isrunning ' });
 });
 
 
